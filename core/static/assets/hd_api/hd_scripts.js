d0395d545fe98e680b80c7515ecf31a560607986
afarde <afarde@kth.se>
2021-02-11 14:14:02 +0100

    page-test mania

--- core/static/assets/hd_api/hd_scripts.js
+++ core/static/assets/hd_api/hd_scripts.js
@@ -1,15 +1,43 @@
-
-
-
 const headers = {
     "Accept": "application/json",
-        "X-Authenticate-User": "cpal",
-            "X-Authenticate-Password": "cpal"
+    "X-Authenticate-User": "cpal",
+    "X-Authenticate-Password": "cpal"
 }
 
 const URL = 'https://p184-geps-production-api.hd-rtls.com/'
 
-function test() {
+hd_scripts = {
+
+
+test: function() {
     const x = 'test2'
     return x
+    },
+
+    test2: function () {
+        fetch('https://p184-geps-production-api.hd-rtls.com/objects/00000001/pos?max_age=60', {
+            method: 'GET', // or 'PUT'
+            headers: {
+                "Accept": "application/json",
+                "X-Authenticate-User": "cpal",
+                "X-Authenticate-Password": "cpal"
+            },
+
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data.Latitude);
+                var x = data.Latitude.value
+                return x;
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+                return error;
+            });
+
+    }
+
+
+
+
 }
